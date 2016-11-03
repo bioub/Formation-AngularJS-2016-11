d73a77c5cb949e3b7a498ca13f176519fde48879
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2016-11-03 12:15:56 +0100

    Jour 1.5

---
+++ AngularJS/03-controller.js
@@ -0,0 +1,19 @@
+(function() {
+    // Création d'un module
+    var app = angular.module('app', [])
+
+    // Récupère un module existant
+    angular.module('app')
+
+    // Injection de dépendance ($scope existe et 
+    // nous est fourni par angular)
+    app.controller('PrenomCtrl', ['$scope', function($scope) {
+        $scope.prenoms = ['Jean', 'Eric'];
+
+        $scope.ajouter = function() {
+            $scope.prenoms.push($scope.prenom);
+            $scope.prenom = '';
+        };
+    }]);
+
+}());
